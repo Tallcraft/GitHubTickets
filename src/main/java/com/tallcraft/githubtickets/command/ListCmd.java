cee4ce251194f9d5f8cd06323f8db21911e7f566
Trikolon <git@pbz.im>
2019-09-06 18:46:51 +0200

    Allow players to view a list of their own opened tickets

--- src/main/java/com/tallcraft/githubtickets/command/ListCmd.java
+++ src/main/java/com/tallcraft/githubtickets/command/ListCmd.java
@@ -4,9 +4,11 @@ import com.tallcraft.githubtickets.ticket.Ticket;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import org.bukkit.entity.Player;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListCmd extends AsyncCommand {
 
@@ -24,6 +26,22 @@ public class ListCmd extends AsyncCommand {
         }
         runSync(() -> {
             replySync(ticketListHeading);
+            if (sender instanceof Player && !hasPermSync("show.all")) {
+                Player player = (Player) sender;
+                List<Ticket> ticketListFiltered = ticketList.stream()
+                        .filter((ticket) -> ticket.getPlayerUUID()
+                                .equals(player.getUniqueId())).collect(Collectors.toList());
+                if (ticketListFiltered.isEmpty()) {
+                    replySync("You don't have any open tickets");
+                    return;
+                }
+                replySync(Ticket.ticketListToChat(ticketListFiltered));
+                return;
+            }
+            if (ticketList.isEmpty()) {
+                replySync("No open tickets");
+                return;
+            }
             replySync(Ticket.ticketListToChat(ticketList));
         });
     }
